4c38fdad86aed74be14737451724b89ceadfeaa4
Olayinka Jet Dosumu <hack.bitcoin@yahoo.com>
2023-01-25 06:23:33 -0800

    p r i n t f c

--- printf.c
+++ printf.c
@@ -1,9 +1,5 @@
 #include "holberton.h"
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
+
 int _printf(const char * const format, ...)
 {
 	convert_match m[] = {
